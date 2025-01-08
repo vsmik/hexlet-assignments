53287ea4f1eff8a4e0886e4ddeda929f0d0f3573
@hexlet/cli <support@hexlet.io>
2025-01-08 15:21:24 +0300

    submit java-oop-ru/subtyping

--- java-oop-ru/subtyping/src/main/java/exercise/FileKV.java
+++ java-oop-ru/subtyping/src/main/java/exercise/FileKV.java
@@ -1,5 +1,43 @@
 package exercise;
 
-// BEGIN
+import java.util.Map;
 
+import static exercise.Utils.*;
+
+// BEGIN
+class FileKV implements KeyValueStorage {
+    Map<String, String> data;
+    String path;
+
+    public FileKV (String path, Map<String, String> data) {
+        this.path = path;
+        data.forEach(this::set);
+    }
+
+    @Override
+    public void set(String key, String value) {
+        Map<String, String> map = toMap();
+        map.put(key, value);
+        writeFile(path, serialize(map));
+    }
+
+    @Override
+    public void unset(String key) {
+        Map<String, String> map = toMap();
+        map.remove(key);
+        writeFile(path, serialize(map));
+    }
+
+    @Override
+    public String get(String key, String defaultValue) {
+        Map<String, String> map = toMap();
+        return map.getOrDefault(key, defaultValue);
+    }
+
+    @Override
+    public Map<String, String> toMap() {
+        String content = readFile(path);
+        return deserialize(content);
+    }
+}
 // END
--- java-oop-ru/subtyping/src/test/java/exercise/FileKVTest.java
+++ java-oop-ru/subtyping/src/test/java/exercise/FileKVTest.java
@@ -6,9 +6,13 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
-import com.fasterxml.jackson.databind.ObjectMapper;
-// BEGIN
 
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+// BEGIN
+import static org.assertj.core.api.Assertions.assertThat;
+import java.util.Map;
 // END
 
 
@@ -24,6 +28,21 @@ class FileKVTest {
     }
 
     // BEGIN
-    
+    @Test
+    void testFileKV() {
+        KeyValueStorage storage = new FileKV(filepath.toString(), Map.of("key", "value"));
+        assertThat(storage.get("somekey", "default")).isEqualTo("default");
+        assertThat(storage.get("key", "default")).isEqualTo("value");
+
+        storage.set("key2", "value2");
+        storage.set("key", "new value");
+        assertThat(storage.get("key2", "default")).isEqualTo("value2");
+        assertThat(storage.get("key", "default")).isEqualTo("new value");
+
+        storage.unset("key");
+        assertThat(storage.get("key", "default")).isEqualTo("default");
+
+        assertThat(storage.toMap()).isEqualTo(Map.of("key2", "value2"));
+    }
     // END
 }
