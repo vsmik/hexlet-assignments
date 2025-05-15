d388a353e4a397feadd47453f9f1a3003a633a9d
@hexlet/cli <support@hexlet.io>
2025-05-15 08:09:01 +0300

    submit java-web-ru/crud

---
+++ java-web-ru/crud/src/main/java/exercise/controller/PostsController.java
@@ -0,0 +1,30 @@
+package exercise.controller;
+
+import static io.javalin.rendering.template.TemplateUtil.model;
+
+import exercise.dto.posts.PostsPage;
+import exercise.dto.posts.PostPage;
+import exercise.repository.PostRepository;
+
+import io.javalin.http.Context;
+import io.javalin.http.NotFoundResponse;
+
+public class PostsController {
+
+    // BEGIN
+    public static void index(Context ctx) {
+        var numPage = ctx.queryParamAsClass("page", Integer.class).getOrDefault(1);
+        var posts = PostRepository.findAll(numPage, 5);
+        var page = new PostsPage(posts, numPage);
+        ctx.render("posts/index.jte", model("page", page));
+    }
+
+    public static void show(Context ctx) {
+        var id = ctx.pathParamAsClass("id", Long.class).get();
+        var post = PostRepository.find(id)
+                .orElseThrow(() -> new NotFoundResponse("Page not found"));
+        var page = new PostPage(post);
+        ctx.render("posts/show.jte", model("page", page));
+    }
+    // END
+}
